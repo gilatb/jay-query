25c7b5a690cd72ad99396d5e8d44b96cf7d064f1
Arol <arolet@gmail.com>
2019-05-29 09:52:02 +0200

    upgrade to v1.4.3

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,16 @@ const gulp = require('gulp');
 const fs = require('fs');
 const del = require('del');
 const $ = require('gulp-load-plugins')();
+const browserSync = require('browser-sync').create();
+
+function serve () {
+  browserSync.init({
+    server: { baseDir: './' }
+  });
+  gulp
+    .watch(['index.js', 'index.html', 'style.css'])
+    .on('change', browserSync.reload);
+}
 
 // Utility to ignore unnecessary files
 // when generating the glob patterns array for gulp.src()
@@ -18,32 +28,41 @@ function addDefSrcIgnore (srcArr) {
 
 // JavaScript and JSON linter
 function lintJs () {
-  return gulp.src(addDefSrcIgnore(['**/*.js', '**/*.json']), {dot: true})
-    .pipe($.eslint({dotfiles: true}))
+  return gulp
+    .src(addDefSrcIgnore(['**/*.js', '**/*.json']), { dot: true })
+    .pipe($.eslint({ dotfiles: true }))
     .pipe($.eslint.format())
     .pipe($.eslint.failAfterError());
 }
 
 // HTML linter
 function lintHtml () {
-  return gulp.src(addDefSrcIgnore(['**/*.html']))
-    .pipe($.htmllint({config: '.htmllintrc.json', failOnError: true}));
+  return gulp
+    .src(addDefSrcIgnore(['**/*.html']))
+    .pipe($.htmllint({ config: '.htmllintrc.json', failOnError: true }));
 }
 
 // CSS linter
 function lintCss () {
-  return gulp.src(addDefSrcIgnore(['**/*.css']))
-    .pipe($.stylelint({
+  return gulp.src(addDefSrcIgnore(['**/*.css'])).pipe(
+    $.stylelint({
       failAfterError: true,
-      reporters: [{formatter: 'string', console: true}]
-    }));
+      reporters: [{ formatter: 'string', console: true }]
+    })
+  );
 }
 
 // Remove solutions from exercises
 function removeSolutions () {
   del.sync('dist');
-  return gulp.src(addDefSrcIgnore(['**']), {dot: true})
-    .pipe($.replace(/^\s*(\/\/|<!--|\/\*)\s*REMOVE-START[\s\S]*?REMOVE-END\s*(\*\/|-->)?\s*$/gm, ''))
+  return gulp
+    .src(addDefSrcIgnore(['**']), { dot: true })
+    .pipe(
+      $.replace(
+        /^\s*(\/\/|<!--|\/\*)\s*REMOVE-START[\s\S]*?REMOVE-END\s*(\*\/|-->)?\s*$/gm,
+        ''
+      )
+    )
     .pipe(gulp.dest('dist'));
 }
 
@@ -55,20 +74,18 @@ function updateConfigForSlave (done) {
   const esLintConfig = require('./.eslintrc.json');
   esLintConfig.rules['no-undef'] = 'off';
   esLintConfig.rules['no-unused-vars'] = 'off';
-  fs.writeFileSync('dist/.eslintrc.json', JSON.stringify(esLintConfig, null, 2));
+  fs.writeFileSync(
+    'dist/.eslintrc.json',
+    JSON.stringify(esLintConfig, null, 2)
+  );
 
   done();
 }
 
 // Lint all files
-exports.lint = gulp.parallel(
-  lintJs,
-  lintHtml,
-  lintCss
-);
+exports.lint = gulp.parallel(lintJs, lintHtml, lintCss);
 
 // Prepare for distribution to students
-exports.dist = gulp.series(
-  removeSolutions,
-  updateConfigForSlave
-);
+exports.dist = gulp.series(removeSolutions, updateConfigForSlave);
+
+exports.default = serve;
